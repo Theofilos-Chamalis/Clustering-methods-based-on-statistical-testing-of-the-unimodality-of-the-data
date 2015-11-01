22e04a0d8f47f9f6e5adc36863b346b1e02e3cb3
Theofilos-Chamalis <theofxam@gmail.com>
2015-11-01 14:20:01 +0200

    Added a small description in the 2D dataset plotter script

--- Plots2D_Of_Datasets/Dataset2Dplotter.m
+++ Plots2D_Of_Datasets/Dataset2Dplotter.m
@@ -1,1 +1,10 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%------------
+% This script plots the 2 dimensional datasets to provide
+% a visualization of the data structure.
+%------------
+% Copyright (C) 2014-2015, Chamalis Theofilos.
+%------------
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 plot(X(:,1),X(:,2), '.');
